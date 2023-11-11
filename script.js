4bddb0f90d770c6edd25fc026fdce6d03aedb8b7
Hrexandro <marcin_nowak_hrx@o2.pl>
2023-11-11 21:18:20 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,114 @@
+
+import {
+  dziczSpotkania,
+  dziczSpotkaniaNaRzece
+} from "./vorpal.js";
+
+const generateButton = document.getElementById("generate-button");
+const nameDisplay = document.getElementById("name-display");
+const categoryPicker = document.getElementById("kategoria");
+const numberPicker = document.getElementById("liczba");
+const formContainer = document.getElementById("form-container");
+const selectColumn = document.getElementById("select-column");
+
+
+
+categoryPicker.addEventListener("change", (e) => {
+  document.getElementsByTagName("footer")[0].classList.add("invisible");
+  removeAllChildren(nameDisplay);
+  updateSecondarySelectStatus();
+});
+
+function updatePick() {
+  category = categoryPicker.value;
+  numberGenerated = numberPicker.value;
+}
+
+let numberGenerated = 20;
+let category = "dziczSpotkania";
+
+function k(sides, exploding = false) {
+  let result = Math.floor(Math.random() * sides) + 1;
+  if (exploding === true) {
+    if (result === sides) {
+      result = result + k(sides, true);
+    }
+  }
+
+  return result;
+}
+
+class Roll {
+  constructor(result) {
+    this.result = result;
+  }
+}
+
+
+
+
+
+
+function removeAllChildren(element) {
+  const counter = element.children.length;
+  for (let m = 0; m <= counter; m++) {
+    if (element.children[0]) {
+      element.children[0].remove();
+    }
+  }
+}
+
+function randomizeFromArray(array) {
+  return array[Math.floor(Math.random() * array.length)];
+}
+
+function displayArray(ar, parent) {
+  for (let j = 0; j < ar.length; j++) {
+    const tableRow = document.createElement("tr");
+    parent.appendChild(tableRow);
+
+    const line = document.createElement("p");
+    line.innerText = `${j + 1}. ${ar[j]}`;
+    tableRow.appendChild(line);
+  }
+}
+
+function pickFromList(pickedList) {
+  if (pickedList.type === "mixerSpaced") {
+    const nonTypeKeys = Object.keys(pickedList).filter((key) => {
+      return key !== "type";
+    });
+    let combinedParts = "";
+    for (let k = 0; k < nonTypeKeys.length; k++) {
+      combinedParts += ` ${randomizeFromArray(pickedList[nonTypeKeys[k]])}`;
+    }
+    return combinedParts;
+  } else if (pickedList.type === "mixerConcatenated") {
+    const nonTypeKeys = Object.keys(pickedList).filter((key) => {
+      return key !== "type";
+    });
+    let combinedParts = "";
+    for (let k = 0; k < nonTypeKeys.length; k++) {
+      combinedParts += randomizeFromArray(pickedList[nonTypeKeys[k]]);
+    }
+    return combinedParts;
+  } else if (pickedList.type === "picker") {
+    return randomizeFromArray(pickedList.list);
+  } else if (pickedList().type === "pickerRoller") {
+    // pickerRollers (e.g. random encounters, corpse loot) are functions, so that the numbers are rerolled each time
+    return randomizeFromArray(pickedList().list);
+  }
+}
+
+generateButton.addEventListener("click", () => {
+  updatePick();
+  const result = [];
+
+  const pickedCategory = eval(category);
+  removeAllChildren(nameDisplay);
+  for (let i = 0; i < numberGenerated; i++) {
+    result.push(pickFromList(pickedCategory));
+  }
+  displayArray(result, nameDisplay);
+});
+
---
+++ vorpal.js
@@ -0,0 +1,48 @@
+export const dziczSpotkania = {
+  type: "picker",
+  list: [
+    "Błędne ogniki prowadzą na śmierć",
+"Drużyna zabłądziła na losowe pole przyległe do docelowego",
+"Frywolne duszki szukają rozrywki",
+"Goniec wiezie ważne wieści",
+"Maruderzy szukają łatwych ofiar",
+"Myśliwy tropi groźnego potwora",
+"Negocjujące sekty – obie szukają wiernych",
+"Niespokojne duchy szukają ukojenia",
+"Okultyści odprawiają rytuał",
+"Partyzanci przygotowują zasadzkę",
+"Plądrowana osada – zarówno zbójcy, jak i ofiary przyjmą wsparcie",
+"Potwór atakuje każdego w zasięgu wzroku",
+"Potwór z młodymi broni potomstwa",
+"Pustelnik szuka odosobnienia",
+"Sprzeczający się przyjaciele – obaj szukają poparcia",
+"Uchodźcy szukają pomocy w przeprawie",
+"Walczące oddziały – oba szukają wsparcia",
+"Zielarz szuka rzadkich ziół",
+"Zwiadowcy starają się przemknąć niepostrzeżenie",
+"Żołnierz szuka pomocy medycznej", 
+  ],
+};
+
+export const dziczSpotkaniaNaRzece = {
+  type: "picker",
+  list: ["Bobry budują tamę",
+  "Drwale spławiają drewno",
+  "Flisacy naprawiają tratwę",
+  "Kapłani dokonują rytualnych ablucji",
+  "Mielizna nieoznaczona na mapach",
+  "Potwór wynurza się z głębin",
+  "Robotnicy budują most",
+  "Wędkarz przysnął na brzegu",
+  "Wędrowiec tonie",
+  "Wodospad nieoznaczony na mapach",
+  
+  ],
+};
+
+
+// export const dziczSpotkania = {
+//   type: "picker",
+//   list: [
+//   ],
+// };
